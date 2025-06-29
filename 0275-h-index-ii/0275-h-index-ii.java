b39cf5a7e0cb5bd97823ac9809ff8d7e00c35580
Neelkanth Dabhi <neelkanth@dal.ca>
2025-06-29 14:14:08 -0300

    Time: 1 ms (26.14%), Space: 47.8 MB (80.35%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] >= citations.length - i) {
+                return citations.length - i;
+            }
+        }
+        return 0;
+    }
+}
