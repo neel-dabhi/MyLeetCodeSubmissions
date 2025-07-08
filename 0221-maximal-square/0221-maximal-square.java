25568bcaa370516829c28803a5e3d1e96e9d7484
Neelkanth Dabhi <neelkanth@dal.ca>
2025-07-08 18:25:52 -0300

    Time: 7 ms (89.42%), Space: 59.7 MB (82.8%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.java
@@ -0,0 +1,22 @@
+// TC: O(3n) -> O(n)
+// SC: O(n)
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int result = 0;
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i + 1][j], dp[i][j + 1]), dp[i + 1][j + 1]) + 1;
+                    result = Math.max(dp[i][j], result);
+                }
+
+            }
+        }
+
+        return result * result;
+    }
+}
