c9c869e3d9711859096078cf173a57db69730087
Neelkanth Dabhi <neelkanth@dal.ca>
2025-07-31 15:28:47 -0300

    Time: 4 ms (22.98%), Space: 45.9 MB (55.29%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,27 @@
+// TC: O(3n)
+// SC: O(n)
+class Solution {
+    public int candy(int[] ratings) {
+        int totalCandies = 0;
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = Math.max(candies[i], candies[i+1] +1);
+            }
+        }
+
+        for(int i=0; i< candies.length; i++){
+           totalCandies = totalCandies + candies[i];
+        }
+
+        return totalCandies;
+    }
+}
