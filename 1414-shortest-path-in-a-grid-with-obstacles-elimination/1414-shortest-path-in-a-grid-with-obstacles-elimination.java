2db0426e207d3a286d7e80fd19c0a132d977add7
Neelkanth Dabhi <neelkanth@dal.ca>
2025-08-04 15:26:40 -0300

    Time: 4 ms (99.1%), Space: 42.9 MB (96.65%) - LeetHub

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -3,41 +3,46 @@ class Solution {
         int steps = 0;
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][][] visited = new boolean[m][n][k + 1];
+        int[][] kCount = new int[m][n];
+
+        for(int[] row: kCount){
+            Arrays.fill(row, -1);
+        }
+
         Queue<int[]> bfs = new LinkedList<>();
 
         int[][] dirs = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
 
         bfs.add(new int[] { 0, 0, k });
-        visited[0][0][k] = true;
+        kCount[0][0] = k;
 
-        if (k >= m + n - 2){
-            return m + n - 2;
-        }
-        
+        if (k >= m + n - 2) return m + n - 2;
+
         while (!bfs.isEmpty()) {
 
             int size = bfs.size();
-            
+
             for (int i = 0; i < size; i++) {
                 int[] curr = bfs.poll();
                 int r = curr[0];
                 int c = curr[1];
                 int currK = curr[2];
 
+
                 if (r == m - 1 && c == n - 1) {
                     return steps;
                 }
-
+                
                 for (int[] dir : dirs) {
                     int x = r + dir[0];
                     int y = c + dir[1];
 
                     if (x >= 0 && y >= 0 && y < n && x < m) {
-                        int temp = currK - grid[r][c];
-                        if (temp >=0 && !visited[x][y][temp]) {
-                            bfs.add(new int[] { x, y, temp });
-                            visited[x][y][temp] = true;
+                        int newK = currK - grid[x][y];
+
+                        if (newK>=0 && newK > kCount[x][y]) {
+                            bfs.add(new int[] { x, y, newK });
+                            kCount[x][y] = newK;
                         }
                     }
                 }
