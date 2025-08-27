4b6f130bd4eeacf10c1a5508a4d4cdb0abab0e86
Neelkanth Dabhi <neelkanth@dal.ca>
2025-08-27 16:22:39 -0300

    Time: 0 ms (100%), Space: 40.4 MB (73.7%) - LeetHub

---
+++ 1033-broken-calculator/1033-broken-calculator.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+
+        int result = 0;
+
+        while (startValue < target) {
+            if (target % 2 == 0) {
+                target = target / 2;
+            } else {
+                target = target + 1;
+            }
+            result++;
+        }
+
+        return result + startValue - target;
+
+    }
+}
