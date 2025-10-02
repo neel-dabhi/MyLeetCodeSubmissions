80428b0075847d55325847593d2821018ee71dbe
Neelkanth Dabhi <neelkanth@dal.ca>
2025-10-02 14:09:18 -0300

    Time: 3 ms (51.54%), Space: 41.9 MB (48.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,20 @@
-// TC: O(n)
-// SC: O(n)
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
 
-        for (char ch : s.toCharArray()) {
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
 
             if (ch == '(') {
                 st.push(')');
-            } else if (ch == '{') {
-                st.push('}');
             } else if (ch == '[') {
                 st.push(']');
+            } else if (ch == '{') {
+                st.push('}');
             } else if (st.isEmpty() || ch != st.pop()) {
                 return false;
             }
         }
-
         return st.isEmpty();
     }
 }
