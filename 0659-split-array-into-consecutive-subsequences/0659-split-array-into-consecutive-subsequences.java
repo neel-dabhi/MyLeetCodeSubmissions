c6e7d61841a3373bab25bea672cf0db24ee2e52a
Neelkanth Dabhi <neelkanth@dal.ca>
2025-10-14 16:09:59 -0300

    Time: 31 ms (13.32%), Space: 47.2 MB (6.77%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean isPossible(int[] nums) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        HashMap<Integer, Integer> canJoinMap = new HashMap<>();
+
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : nums) {
+            if (canJoinMap.containsKey(num) && freqMap.containsKey(num)) {
+
+                // num can join existing subsequences
+                freqMap.put(num, freqMap.get(num) - 1);
+
+                if (freqMap.get(num) == 0) {
+                    freqMap.remove(num);
+                }
+
+                canJoinMap.put(num, canJoinMap.get(num) - 1);
+                if (canJoinMap.get(num) == 0) {
+                    canJoinMap.remove(num);
+                }
+
+                // create num + 1 in canJoinMap
+                canJoinMap.put(num + 1, canJoinMap.getOrDefault(num + 1, 0) + 1);
+
+            } else if (freqMap.containsKey(num)) {
+                // if num cant join existing, check if it can build its own subsequences
+                if (freqMap.containsKey(num + 1) && freqMap.containsKey(num + 2)) {
+                    // can build their own subsequence
+                    freqMap.put(num, freqMap.get(num) - 1);
+                    freqMap.put(num + 1, freqMap.get(num + 1) - 1);
+                    freqMap.put(num + 2, freqMap.get(num + 2) - 1);
+
+                    // delete if zero
+                    if (freqMap.get(num) == 0) {
+                        freqMap.remove(num);
+                    }
+                    if (freqMap.get(num + 1) == 0) {
+                        freqMap.remove(num + 1);
+                    }
+                    if (freqMap.get(num + 2) == 0) {
+                        freqMap.remove(num + 2);
+                    }
+
+                    // put num + 3 in canJoinMap
+                    canJoinMap.put(num + 3, canJoinMap.getOrDefault(num + 3, 0) + 1);
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
