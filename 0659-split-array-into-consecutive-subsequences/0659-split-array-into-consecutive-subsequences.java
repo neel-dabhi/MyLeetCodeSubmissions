cb350ac2ed3a3ccf045fcfaab27d2615400a99ea
Neelkanth Dabhi <neelkanth@dal.ca>
2025-10-14 16:11:36 -0300

    Time: 30 ms (16.28%), Space: 47 MB (6.77%) - LeetHub

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.java
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.java
@@ -9,18 +9,15 @@ class Solution {
 
         for (int num : nums) {
             if (canJoinMap.containsKey(num) && freqMap.containsKey(num)) {
-
                 // num can join existing subsequences
-                freqMap.put(num, freqMap.get(num) - 1);
 
-                if (freqMap.get(num) == 0) {
+                freqMap.put(num, freqMap.get(num) - 1);
+                if (freqMap.get(num) == 0)
                     freqMap.remove(num);
-                }
 
                 canJoinMap.put(num, canJoinMap.get(num) - 1);
-                if (canJoinMap.get(num) == 0) {
+                if (canJoinMap.get(num) == 0)
                     canJoinMap.remove(num);
-                }
 
                 // create num + 1 in canJoinMap
                 canJoinMap.put(num + 1, canJoinMap.getOrDefault(num + 1, 0) + 1);
@@ -34,19 +31,17 @@ class Solution {
                     freqMap.put(num + 2, freqMap.get(num + 2) - 1);
 
                     // delete if zero
-                    if (freqMap.get(num) == 0) {
+                    if (freqMap.get(num) == 0)
                         freqMap.remove(num);
-                    }
-                    if (freqMap.get(num + 1) == 0) {
+                    if (freqMap.get(num + 1) == 0)
                         freqMap.remove(num + 1);
-                    }
-                    if (freqMap.get(num + 2) == 0) {
+                    if (freqMap.get(num + 2) == 0)
                         freqMap.remove(num + 2);
-                    }
 
                     // put num + 3 in canJoinMap
                     canJoinMap.put(num + 3, canJoinMap.getOrDefault(num + 3, 0) + 1);
-                }else{
+                } else {
+                    // cant join existing or make new 
                     return false;
                 }
             }
