a0ec13c20bd50640432da7609ed3031232144034
Neelkanth Dabhi <neelkanth@dal.ca>
2025-10-24 19:48:47 -0300

    Time: 8 ms (8.67%), Space: 45.8 MB (10.01%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,38 +14,41 @@
  * }
  */
 class Solution {
-    int idx=0;
-    Map<Integer, Integer> inorderLookup = new HashMap<>();
-
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        // [3,9,20,15,7]
+        // [9,3,15,20,7]
+        int n = inorder.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i<inorder.length; i++) {
-            inorderLookup.put(inorder[i], i);
+        for (int i = 0; i < n; i++) {
+            map.put(inorder[i], i);
         }
-
-        return helper(preorder, inorderLookup, 0, preorder.length - 1);
+        return helper(map, preorder, inorder);
     }
 
-    private TreeNode helper(int[] preorder,Map<Integer, Integer> inorderLookup, int start, int end) {
-
-        if (end < start) {
+    private TreeNode helper(HashMap<Integer, Integer> map, int[] preorder, int[] inorder) {
+        if (preorder.length == 0) {
             return null;
         }
 
-        int rootValue = preorder[idx];
-        TreeNode root = new TreeNode(rootValue);
-        idx++;
-        int rootIndex = inorderLookup.get(rootValue);
-        
+        int rootVal = preorder[0];
+        int rootIdx = -1;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == rootVal) {
+                rootIdx = i;
+            }
+        }
 
-        int startLeft = start;
-        int endLeft = rootIndex - 1;
-        int startRight = rootIndex + 1;
-        int endRight = end;
+        int[] leftInOrder = Arrays.copyOfRange(inorder, 0, rootIdx);
+        int[] rightInOrder = Arrays.copyOfRange(inorder, rootIdx + 1, inorder.length);
+        int[] leftPreOrder = Arrays.copyOfRange(preorder, 1, 1 + leftInOrder.length);
+        int[] rightPreOrder = Arrays.copyOfRange(preorder, 1 + leftInOrder.length, preorder.length);
 
-        root.left = helper(preorder, inorderLookup, startLeft, endLeft);
-        root.right = helper(preorder, inorderLookup, startRight, endRight);
+        TreeNode left = helper(map, leftPreOrder, leftInOrder);
+        TreeNode right = helper(map, rightPreOrder, rightInOrder);
 
-        return root;
+        return new TreeNode(rootVal, left, right);
+
     }
+
 }
