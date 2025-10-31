a0db3c647aa40edd70746faed5664f53207f3087
Neelkanth Dabhi <neelkanth@dal.ca>
2025-10-31 11:32:04 -0300

    Time: 2 ms (65.18%), Space: 44.1 MB (5.49%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,51 +1,56 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<Integer> row = new LinkedList<Integer>();
-        Queue<Integer> col = new LinkedList<Integer>();
-        int countFresh = 0;
         int time = 0;
+        int fresh = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        Queue<int[]> bfs = new LinkedList<>();
+        int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 
-        int[][] dirs = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        // get fresh count and prime the bfs queue
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    fresh++;
+                }
                 if (grid[i][j] == 2) {
-                    row.add(i);
-                    col.add(j);
-                } else if (grid[i][j] == 1) {
-                    countFresh++;
+                    bfs.add(new int[] { i, j });
                 }
             }
         }
-
-        if (countFresh == 0)
+        // if no fresh it all rotten at t0
+        if (fresh == 0) {
             return 0;
+        }
 
-        while (!row.isEmpty()) {
-            int size = row.size();
+        while (!bfs.isEmpty()) {
+            int size = bfs.size();
 
             for (int i = 0; i < size; i++) {
-                int currRow = row.poll();
-                int currCol = col.poll();
+                int[] curr = bfs.poll();
+
+                int x = curr[0];
+                int y = curr[1];
 
                 for (int[] dir : dirs) {
-                    int r = currRow + dir[0];
-                    int c = currCol + dir[1];
-                    if (r < grid.length && c < grid[0].length && c >= 0 && r >= 0 && grid[r][c] == 1) {
-                        row.add(r);
-                        col.add(c);
-                        grid[r][c] = 2;
-                        countFresh--;
+                    int row = x + dir[0];
+                    int col = y + dir[1];
+
+                    // bounds check, fesh check
+                    if (row >= 0 && col >= 0 && row < n && col < m && grid[row][col] == 1) {
+                        bfs.add(new int[] { row, col });
+                        grid[row][col] = 2;
+                        fresh--;
+
+                        if (fresh == 0) {
+                            return time + 1;
+                        }
                     }
                 }
             }
             time++;
         }
 
-        if (countFresh == 0) {
-            return time - 1;
-        } else {
-            return -1;
-        }
+        return -1;
     }
 }
