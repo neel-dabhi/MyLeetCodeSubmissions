bd6a57d89b436d093d7751c4811d1335558e8cd0
Neelkanth Dabhi <neelkanth@dal.ca>
2025-11-09 16:00:06 -0400

    Time: 10 ms (89.87%), Space: 65.6 MB (6.18%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,65 +1,57 @@
 class Solution {
-    TrieNode root;
-
     class TrieNode {
-        Character val;
-        boolean isEnd;
-        TrieNode[] children;
-
-        TrieNode(char ch) {
-            this.val = ch;
-            this.isEnd = false;
-            this.children = new TrieNode[26];
-        }
+        boolean isEnding = false;
+        TrieNode[] children = new TrieNode[26];
     }
 
+    TrieNode root;
+
     public String replaceWords(List<String> dictionary, String sentence) {
-        root = new TrieNode(' ');
-        for (String word : dictionary) {
-            insert(word);
+        root = new TrieNode();
+
+        for(String word : dictionary){
+            insertToTrie(word);
         }
 
         String[] words = sentence.split(" ");
 
-        for (int j = 0; j < words.length; j++) {
-            TrieNode current = root;
-            String word = words[j];
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < word.length(); i++) {
-                char ch = word.charAt(i);
-
-                if (current.children[ch - 'a'] == null || current.isEnd) {
-                    break;
-                }
-
-                sb.append(ch);
-                current = current.children[ch - 'a'];
-            }
-            if (current.isEnd) {
-                words[j] = sb.toString();
-            }
-
-        }
-
         StringBuilder result = new StringBuilder();
-        for (String word : words) {
-            result.append(word);
-            result.append(" ");
+        for(String word : words){
+            result.append(findFirstPreFix(word));
+            result.append( " ");
         }
-
         return result.toString().trim();
     }
 
-    private void insert(String word) {
-        TrieNode current = root;
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
+    private void insertToTrie(String word) {
+        TrieNode curr = root;
+        for (char ch : word.toCharArray()) {
 
-            if (current.children[ch - 'a'] == null) {
-                current.children[ch - 'a'] = new TrieNode(ch);
+            if (curr.children[ch - 'a'] == null) {
+                curr.children[ch - 'a'] = new TrieNode();
             }
-            current = current.children[ch - 'a'];
+            curr = curr.children[ch - 'a'];
         }
-        current.isEnd = true;
+        curr.isEnding = true;
+    }
+
+    private String findFirstPreFix(String word){
+        TrieNode curr = root;
+        StringBuilder prefix = new StringBuilder();
+
+        for(char ch: word.toCharArray()){
+            if(curr.children[ch - 'a'] == null){
+                return word;
+            }
+            curr = curr.children[ch - 'a'];
+            prefix.append(ch);
+
+            if( curr.isEnding == true){
+                return prefix.toString();
+            }
+            
+        }
+
+        return word;
     }
 }
