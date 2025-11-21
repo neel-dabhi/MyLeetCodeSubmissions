c9c028aac00111b4df7cbd625af1296e9ecdc6d9
Neelkanth Dabhi <neelkanth@dal.ca>
2025-11-21 14:37:34 -0400

    Time: 13 ms (89.8%), Space: 46.2 MB (76.74%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,52 +1,35 @@
-// TC : O(n)
-// SC : O(n)
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> st = new Stack<>();
-        int currNum = 0;
-        int lastSign = '+';
+        int solution = 0;
+        char lastSign = '+';
+        int tail = 0;
+        int curr = 0;
+
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-
             if (Character.isDigit(ch)) {
-                currNum = currNum * 10 + ch - '0';
-            } else if (!Character.isDigit(ch) && ch != ' ') {
+                curr = curr * 10 + (ch - '0');
+            }
+            if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
                 if (lastSign == '+') {
-                    st.push(currNum);
+                    solution = solution + curr;
+                    tail = +curr;
                 } else if (lastSign == '-') {
-                    st.push(-currNum);
+                    solution = solution - curr;
+                    tail = -curr;
                 } else if (lastSign == '*') {
-                    int popped = st.pop();
-                    st.push(popped * currNum);
-                    System.out.println(st.peek());
+                    solution = solution - tail + (tail * curr);
+                    tail = tail * curr;
                 } else if (lastSign == '/') {
-                    int popped = st.pop();
-                    st.push(popped / currNum);
+                    solution = solution - tail + tail / curr;
+                    tail = tail / curr;
                 }
-                currNum = 0;
+                curr = 0;
                 lastSign = ch;
             }
+
         }
 
-        if (lastSign == '+') {
-            st.push(currNum);
-        } else if (lastSign == '-') {
-            st.push(-currNum);
-        } else if (lastSign == '*') {
-            int popped = st.pop();
-            st.push(popped * currNum);
-            System.out.println(st.peek());
-        } else if (lastSign == '/') {
-            int popped = st.pop();
-            st.push(popped / currNum);
-        }
-        
-
-        int result = 0;
-        while (!st.isEmpty()) {
-            System.out.println(st.peek());
-            result = result + st.pop();
-        }
-        return result;
+        return solution;
     }
 }
